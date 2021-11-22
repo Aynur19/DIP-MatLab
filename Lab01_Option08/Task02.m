24845f196c915b0fcee867d41ac61dc615b5f2fe
Aynur19 <nasybullin.aynur@yandex.ru>
2021-11-23 01:48:16 +0300

    added a project for the first laboratory work option 8

---
+++ Lab01_Option08/Task02.m
@@ -0,0 +1,26 @@
+clc;
+close all;
+clear all;
+
+size = 500;
+A = 3;              % amplitude of wave
+lambda = 20;        % wavelength in nanometers
+
+x = linspace(0, size, size);   % vector from 0 to width
+y = linspace(0, size, size);  % vector from 0 to height
+[X, Y] = meshgrid(x , y);       % matrices of x and y values at each position
+
+RW0=RingWave(A, X-(size), Y-(1), lambda);  
+RW1=RingWave(A, X-(1), Y-(size), lambda);  
+
+img = zeros(size);
+img(:, :, 1) = RW0;     % insert red wave
+img(:, :, 3) = RW1;     % insert blu wave
+img = normalize(img, 'range');
+
+figure(); imshow(img, []); title('Red and Blue Waves Interference');
+
+function RW = RingWave(amp, x, y, lambda)
+    r1 = sqrt(x.*x+y.*y);   % dot notation: multiply element-wise
+    RW = amp*cos(2*pi*r1/(lambda));
+end
